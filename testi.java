49bbaed5b908060083ad30b1a1c3538c25fe7737
Junnu Danhammer <junnudanhammer@gmail.com>
2023-03-15 12:15:41 +0200

    Update testi.java

--- testi.java
+++ testi.java
@@ -1,6 +1,7 @@
 public class testi {
     public static void main(String[] args) {
         int[] lista = {1, 2, 3, 4, 5};
+
         for (int i = 0; lista.length < 5; i++ ) {
             while (i < 5) {
                 System.out.println("moikkuli");
@@ -9,3 +10,4 @@ public class testi {
     }
 }
 
+// Ei toimi kuitenkaan
