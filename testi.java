9cdd2d197311337a71c9f7011804877df4480177
Junnu Danhammer <junnudanhammer@gmail.com>
2023-03-15 12:13:16 +0200

    Update testi.java

--- testi.java
+++ testi.java
@@ -1,6 +1,11 @@
 public class testi {
     public static void main(String[] args) {
-        System.out.println("moikkuli");
+        int[] lista = {1, 2, 3, 4, 5};
+        for (int i = 0; lista.length < 5; i++ ) {
+            while (i < 5) {
+                System.out.println("moikkuli");
+            }
+        }
     }
 }
 
